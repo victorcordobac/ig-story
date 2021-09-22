d8fe70737c3e62c8fe303a421fe10a0b9c722482
victorcordoba <hola@victorcordoba.com>
2021-09-22 22:34:49 +0200

    Corrección estilos

--- templates/layout.php
+++ templates/layout.php
@@ -1,28 +1,28 @@
 <?php foreach ($stories as $story) : // loop over each story element 
 ?>
 <?php if ('VIDEO' == $story['media_info']['media_type']) : // story media is a video 
-	?>
+    ?>
 <div style="text-align:center">
-    <div>
+    <div style="text-align:center">
         <video controls poster="<?php echo $story['media_info']['thumbnail_url']; ?>" style="max-width:300px">
             <source src="<?php echo $story['media_info']['media_url']; ?>" />
         </video>
     </div>
     <?php elseif ('IMAGE' == $story['media_info']['media_type']) : // story media is an image 
-		?>
-    <div>
+        ?>
+    <div style="text-align:center">
         <img src="<?php echo $story['media_info']['media_url']; ?>" style="max-width:300px" />
     </div>
     <?php endif; ?>
-    <div>
+    <div style="text-align:center">
         <b>
             <?php echo $story['media_info']['username']; ?>
         </b>
+        <button href="<?php echo $story['media_info']['permalink']; ?>" target="_blank" style="color: black;">
+            Ver en instagram
+            <i class="fa fa-instagram">
+            </i>
+        </button>
     </div>
-    <button href="<?php echo $story['media_info']['permalink']; ?>" target="_blank" style="color: black;">
-        Ver en instagram
-        <i class="fa fa-instagram">
-        </i>
-    </button>
 </div>
 <?php endforeach; ?>
