e9ba645526a5de6a3556b5a89addace679238607
victorcordoba <hola@victorcordoba.com>
2021-09-22 18:52:45 +0200

    Alineamiento center

--- templates/layout.php
+++ templates/layout.php
@@ -2,25 +2,27 @@
 ?>
 <?php if ('VIDEO' == $story['media_info']['media_type']) : // story media is a video 
 	?>
-<div>
-    <video controls poster="<?php echo $story['media_info']['thumbnail_url']; ?>" style="max-width:300px">
-        <source src="<?php echo $story['media_info']['media_url']; ?>" />
-    </video>
+<div style="text-align:center">
+    <div>
+        <video controls poster="<?php echo $story['media_info']['thumbnail_url']; ?>" style="max-width:300px">
+            <source src="<?php echo $story['media_info']['media_url']; ?>" />
+        </video>
+    </div>
+    <?php elseif ('IMAGE' == $story['media_info']['media_type']) : // story media is an image 
+		?>
+    <div>
+        <img src="<?php echo $story['media_info']['media_url']; ?>" style="max-width:300px" />
+    </div>
+    <?php endif; ?>
+    <div>
+        <b>
+            <?php echo $story['media_info']['username']; ?>
+        </b>
+    </div>
+    <button href="<?php echo $story['media_info']['permalink']; ?>" target="_blank" style="color: black;">
+        Ver en instagram
+        <i class="fa fa-instagram">
+        </i>
+    </button>
 </div>
-<?php elseif ('IMAGE' == $story['media_info']['media_type']) : // story media is an image 
-	?>
-<div>
-    <img src="<?php echo $story['media_info']['media_url']; ?>" style="max-width:300px" />
-</div>
-<?php endif; ?>
-<div>
-    <b>
-        <?php echo $story['media_info']['username']; ?>
-    </b>
-</div>
-<button href="<?php echo $story['media_info']['permalink']; ?>" target="_blank" style="color: black;">
-    Ver en instagram
-    <i class="fa fa-instagram">
-    </i>
-</button>
 <?php endforeach; ?>
